1c3e4e77df030d5f7a7ab4c80d9aedc75c917488
Avikant Srivastava <asdavikant@gmail.com>
2024-02-11 23:29:01 +0530

    feat: add game deck and deck

--- models/deck.go
+++ models/deck.go
@@ -1,1 +1,40 @@
 package models
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Deck struct {
+	Cards   []Card
+	Counter map[string]int
+}
+
+func NewDeck() *Deck {
+	return &Deck{
+		Cards:   make([]Card, 0),
+		Counter: make(map[string]int),
+	}
+}
+
+func (d *Deck) Shuffle() {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(d.Cards), func(i, j int) {
+		d.Cards[i], d.Cards[j] = d.Cards[j], d.Cards[i]
+	})
+}
+
+func (d *Deck) AddCard(card Card) {
+	d.Cards = append(d.Cards, card)
+	d.Counter[card.Type()]++
+}
+
+func (d *Deck) RemoveCard(index int) Card {
+	if index < 0 || index >= len(d.Cards) {
+		return Card{} // Return an empty card or handle error
+	}
+	card := d.Cards[index]
+	d.Cards = append(d.Cards[:index], d.Cards[index+1:]...)
+	d.Counter[card.Type()]--
+	return card
+}
--- models/game_deck.go
+++ models/game_deck.go
@@ -1,1 +1,37 @@
 package models
+
+import (
+	color "github.com/AvikantSrivastava/uno/models/constants/color"
+	rank "github.com/AvikantSrivastava/uno/models/constants/rank"
+)
+
+type GameDeck struct {
+	*Deck
+}
+
+func NewGameDeck() *GameDeck {
+	gd := &GameDeck{
+		Deck: NewDeck(),
+	}
+	gd.initColoredCards()
+	gd.initNonColoredCards()
+	gd.Shuffle()
+	return gd
+}
+
+func (gd *GameDeck) initColoredCards() {
+	for _, c := range color.ALLColors {
+		for _, r := range rank.NumberCards {
+			gd.AddCard(Card{Color: c, Rank: r})
+		}
+		for _, r := range rank.ActionCards {
+			gd.AddCard(Card{Color: c, Rank: r})
+		}
+	}
+}
+
+func (gd *GameDeck) initNonColoredCards() {
+	for _, r := range rank.ActionCardsNoColor {
+		gd.AddCard(Card{Color: "", Rank: r})
+	}
+}
