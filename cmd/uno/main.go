5de6fecfb835dfebf31a6d405a273f35af8c9bac
Avikant Srivastava <asdavikant@gmail.com>
2024-02-12 08:53:34 +0530

    feat: add game logic

--- cmd/uno/main.go
+++ cmd/uno/main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/AvikantSrivastava/uno/models"
+	"github.com/AvikantSrivastava/uno/internal"
 )
 
 func main() {
-	var gameDeck = models.NewGameDeck()
-	var player1 = models.NewPlayer("Player 1")
-	var player2 = models.NewPlayer("Player 2")
-
-	fmt.Println("Total cards in the deck before:", gameDeck.NumberOfCards())
-
-	var set1 = gameDeck.Cut(5)
-	var set2 = gameDeck.Cut(9)
-
-	player1.AddCards(set1)
-	player2.AddCards(set2)
-
-	player1.CardInHand()
-	player2.CardInHand()
-
+	players := []string{"avikant", "player2", "player3"}
+	game := internal.NewGame(players)
+	topCard := game.GameDeck.TopCard()
+	fmt.Println(topCard.LogCard())
 }
--- internal/game.go
+++ internal/game.go
@@ -1,1 +1,26 @@
 package internal
+
+import "github.com/AvikantSrivastava/uno/models"
+
+type Game struct {
+	Players  []*models.Player
+	GameDeck *models.GameDeck
+	Reverse  bool
+}
+
+func NewGame(playerNames []string) *Game {
+	players := make([]*models.Player, len(playerNames))
+	for i, name := range playerNames {
+		players[i] = models.NewPlayer(name)
+	}
+	gameDeck := models.NewGameDeck()
+	for _, p := range players {
+		p.AddCards(gameDeck.Cut(7))
+	}
+
+	game := &Game{
+		Players:  players,
+		GameDeck: gameDeck,
+	}
+	return game
+}
