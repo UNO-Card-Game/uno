96df301336fe3dea8673098f5120dd53ac26b650
avikantsrivastava <asdavikant@gmail.com>
2025-01-04 14:01:02 +0100

    json structure not needed at the moment

--- models/commands/drawcard.go
+++ models/commands/drawcard.go
@@ -1,8 +1,5 @@
 package commands
 
-import "uno/models/game"
-
 type DrawCardComamnd struct {
 	NumberofCards int         `json:"number_of_cards"`
-	Player        game.Player `json:"player_id"`
 }
--- models/commands/playcard.go
+++ models/commands/playcard.go
@@ -1,8 +1,5 @@
 package commands
 
-import "uno/models/game"
-
 type PlayCardCommand struct {
 	CardIndex int         `json:"card_index"`
-	Player    game.Player `json:"player_id"`
 }
