dd04fa5f3d9235396aef9f47ba94878b100cf1ed
Mahim Dashora <60029463+mahimdashora@users.noreply.github.com>
2025-02-09 17:14:16 -0500

    Update player.go with drawcard Logic Fixed

--- models/game/player.go
+++ models/game/player.go
@@ -36,13 +36,14 @@ func (player *Player) AddCards(cards []Card) {
 }
 
 func (p *Player) HasPlayableCard(topCard Card) bool {
-
 	for _, card := range p.Deck.Cards {
 		switch {
 		case card.Type() == "action-card" && card.Color == topCard.Color: // DRAW2 ,SKIP ,REVERSE don't have rank
 			return true
-		case card.Type() == "action-card-no-color":
-			return true
+		case topCard.Type() == "action-card-no-color": //When topcard is of action-card-no-color type, return true only if
+			if card.Color == TopColor || card.Type() == "action-card-no-color" { // card deck has matching color or deck has another no-color action card
+				return true
+			}
 		case card.Color == topCard.Color || card.Rank == topCard.Rank:
 			return true
 		}
